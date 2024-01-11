103856cd03802e49aa38a4ee1f36ae0bec650672
newsay <syaswen@gmail.com>
2024-01-10 17:09:00 -0800

    Added title and icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,35 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import budgieLogo from './assets/budgieLogo.png'
 import './App.css'
+import * as React from 'react';
+import CssBaseline from '@mui/material/CssBaseline';
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1 className='budgieTitle'> <img src={budgieLogo} className="budgie" /> Budgie Bot  </h1>
+      <h2>Created with React, TypeScript, Vite & Material UI</h2>
+
+      <Container fixed>
+        <Box sx={{ bgcolor: '#cfe8fc', width: '50vw', height: '100vh' }} >
+          <div className="card">
+            <button onClick={() => setCount((count) => count + 1)}>
+              count is {count}
+            </button>
+            <p>
+              Edit <code>src/App.tsx</code> and save to test HMR
+            </p>
+          </div>
+          <p className="read-the-docs">
+            Click on the Vite and React logos to learn more
+          </p>
+        </Box>
+      </Container>
+
     </>
   )
 }
